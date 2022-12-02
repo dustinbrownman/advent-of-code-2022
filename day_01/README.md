f129c0d41a705d2cc4ae3b2c8c3c39b64ae671ae
Dustin Brown <dustinbrownman@github.com>
2022-12-02 07:09:55 +0000

    Day one part two solution

--- day_01/README.md
+++ day_01/README.md
@@ -2,6 +2,18 @@
 
 [https://adventofcode.com/2022/day/1](https://adventofcode.com/2022/day/1)
 
+To run a solution, run the binary with the solution number you want. If you don't pass in any solution, it will default to the first.
+
+```
+./solution 1
+```
+
 ## Part 1
 
 I had to take some inspiration (straight copying) from the Rust docs to figure out how to iterate over a file line by line. Luckily, I think I'll be able to use that approach for many of these puzzles. Most of the rest was figuring out how to deal with results and optionals. I was really pleased with the `parse_line` function and it's ability to tell if a line had some content or none at all.
+
+## Part 2
+
+I knew I wanted to keep track of the top three "elf packs" as we iterated through rather than trying to sort them at the end, but I wasn't quite sure how to do that. I leaned on Copilot to help me with the syntax, especially uncovering the [`std::iter::Iterator position` method](https://doc.rust-lang.org/stable/std/iter/trait.Iterator.html#method.position). That allowed me to insert a new pack into the correct position of the Vector and then `pop` off any that got pushed to the back.
+
+Also, shout out to [this blog post](https://mkaz.blog/working-with-rust/command-line-args/) that helped me sort my command line arguments. I wanted to be able to run both solutions and this was the most staight-forward example I could find.
--- day_01/solution.rs
+++ day_01/solution.rs
@@ -3,6 +3,21 @@ use std::io::{BufRead, BufReader, Lines, Result};
 use std::path::Path;
 
 fn main() {
+    let mut args = std::env::args().collect::<Vec<String>>();
+
+    args.remove(0);
+
+    match args.pop() {
+        Some(solution) => match solution.as_str() {
+            "1" => part_one_solution(),
+            "2" => part_two_solution(),
+            _ => println!("Unknown solution"),
+        },
+        None => part_one_solution(),
+    }
+}
+
+fn part_one_solution() {
     if let Ok(lines) = read_lines("./input.txt") {
         let mut current_calorie_count: u32 = 0;
         let mut elf_packs = Vec::new();
@@ -20,8 +35,36 @@ fn main() {
         }
 
         let biggest_elf_pack = elf_packs.iter().max().unwrap();
-
         println!("The biggest elf pack has {} calories", biggest_elf_pack);
+    }
+}
+
+fn part_two_solution() {
+    if let Ok(lines) = read_lines("./input.txt") {
+        let mut current_calorie_count: u32 = 0;
+        let mut largest_elf_packs = vec![0; 3];
+
+        for line in lines {
+            if let Ok(line) = line {
+                match parse_line(line) {
+                    Some(item_in_calories) => current_calorie_count += item_in_calories,
+                    None => {
+                        // Look for a pack among the largest that is less than the current one
+                        match largest_elf_packs.iter().position(|&pack| pack < current_calorie_count) {
+                            Some(index) => {
+                                largest_elf_packs.insert(index, current_calorie_count);
+                                largest_elf_packs.pop();
+                            }
+                            None => (),
+                        }
+                        current_calorie_count = 0;
+                    }
+                }
+            }
+        }
+
+        let sum_of_largest_elf_packs: u32 = largest_elf_packs.iter().sum();
+        println!("The sum of the largest elf packs is {}", sum_of_largest_elf_packs);
     }
 }
 
